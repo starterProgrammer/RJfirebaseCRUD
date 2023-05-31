4b0794694ae731cb3b628b4b321d044574f79252
starterProgrammer <ali.talaee.engeneer@gmail.com>
2023-05-31 21:49:50 +0330

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Login } from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} /> 
+      </Routes>
+    </Router>
   );
 }
 
